88574c192cb97cc8e6f0584c0c749b3e1f874f47
Thareeqzrd <ziadramadhan570@gmail.com>
2025-10-31 13:34:23 +0700

    Fix: Ignore .next folder

--- .next/dev/static/chunks/app_page_tsx_3873bb71._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_90f98543._.js",
-  "static/chunks/node_modules_6d74d29f._.js"
-],
-    source: "dynamic"
-});
